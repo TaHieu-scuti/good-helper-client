8b6bffcfe4b34a56892545e9cfddb5b79cbf29e0
TaHieu-scuti <hieutq@scuti.asia>
2019-11-18 18:07:22 +0700

    Updates

---
+++ precache-manifest.9480bca9d9af688656338e6cacbcb3fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54342ecd166d266ffba699203a669890",
+    "url": "/helper/index.html"
+  },
+  {
+    "revision": "a0f19f5e720c930963f1",
+    "url": "/helper/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "3fe0ffb4d7909eb23096",
+    "url": "/helper/static/js/2.495cd4c4.chunk.js"
+  },
+  {
+    "revision": "a0f19f5e720c930963f1",
+    "url": "/helper/static/js/main.89af0c57.chunk.js"
+  },
+  {
+    "revision": "1ea0362ff55f9feaba4f",
+    "url": "/helper/static/js/runtime-main.aa63a44c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/helper/static/media/logo.25bf045c.svg"
+  }
+]);
